163fbefb75e4a651a8ca7275615f8be47296be9c
zalot.zhaoh <zalot.zhaoh@766b8ed6-8c02-485e-ac38-e4d67e22fedb>
2012-02-27 07:00:11 +0000

    新增协议范本
    git-svn-id: http://svn.alibaba-inc.com/repos/ali_platform/havana/sandbox/trunk@149199 766b8ed6-8c02-485e-ac38-e4d67e22fedb

---
+++ src/java/com/alibaba/hbase/replication/protocol/Head.java
@@ -0,0 +1,5 @@
+package com.alibaba.hbase.replication.protocol;
+
+public class Head {
+
+}
---
+++ src/java/com/alibaba/hbase/replication/protocol/Version1.java
@@ -0,0 +1,15 @@
+package com.alibaba.hbase.replication.protocol;
+
+public class Version1 implements MetaData{
+
+	@Override
+	public Head getHead() {
+		return null;
+	}
+
+	@Override
+	public Body getBody() {
+		return null;
+	}
+	
+}
