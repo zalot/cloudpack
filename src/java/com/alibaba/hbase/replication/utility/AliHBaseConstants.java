8fa2a31abd44d04b4fae20077c33728009d6bf57
zalot.zhaoh <zalot.zhaoh@766b8ed6-8c02-485e-ac38-e4d67e22fedb>
2012-03-02 09:57:40 +0000

    git-svn-id: http://svn.alibaba-inc.com/repos/ali_platform/havana/sandbox/trunk@151069 766b8ed6-8c02-485e-ac38-e4d67e22fedb

--- src/java/com/alibaba/hbase/replication/utility/AliHBaseConstants.java
+++ src/java/com/alibaba/hbase/replication/utility/AliHBaseConstants.java
@@ -1,8 +1,22 @@
 package com.alibaba.hbase.replication.utility;
 
 public class AliHBaseConstants {
-    public static String CONFKEY_ZOO_ROOT = "com.alibaba.hbase.replication.zoo.root";
-    public static String ZOO_ROOT         = "/alirep";
+
+    public static String CONFKEY_ZOO_ROOT                = "com.alibaba.hbase.replication.zoo.root";
+    public static String ZOO_ROOT                        = "/alirep";
+
+    public static String CONFKEY_ZOO_SCAN_ROOT           = "com.alibaba.hbase.replication.zoo.root";
+    public static String ZOO_SCAN_ROOT                   = "/alirepscanlock";
+    public static String ZOO_SCAN_LOCK                   = "/scanlock";
+
     public static String ZOO_PERSISTENCE_HLOG_GROUP      = "/aligroups";
     public static String ZOO_PERSISTENCE_HLOG_GROUP_LOCK = "/aliglock";
+
+    public static String PATH_BASE_HLOG                  = ".logs";
+    public static String PATH_BASE_OLDHLOG               = ".oldlogs";
+
+    public static String CONFKEY_ZOO_SCAN_LOCK_SLEEPTIME = "com.alibaba.hbase.replication.zoo.scan.sleeptime";
+    public static String CONFKEY_ZOO_SCAN_LOCK_RETRYTIME = "com.alibaba.hbase.replication.zoo.scan.retrytime";
+    public static long   ZOO_SCAN_LOCK_SLEEPTIME         = 10000;
+    public static long   ZOO_SCAN_LOCK_RETRYTIME         = 30000;
 }
