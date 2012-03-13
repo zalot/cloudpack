6197f3a9ca0c5aa1a31627e5bf7d04136a5d4698
zalot.zhaoh <zalot.zhaoh@766b8ed6-8c02-485e-ac38-e4d67e22fedb>
2012-03-13 14:20:49 +0000

    git-svn-id: http://svn.alibaba-inc.com/repos/ali_platform/havana/sandbox/trunk@154938 766b8ed6-8c02-485e-ac38-e4d67e22fedb

--- src/main/java/com/alibaba/hbase/replication/zookeeper/NothingZookeeperWatch.java
+++ src/main/java/com/alibaba/hbase/replication/zookeeper/NothingZookeeperWatch.java
@@ -2,7 +2,6 @@ package com.alibaba.hbase.replication.zookeeper;
 
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.Watcher.Event.KeeperState;
 
 /**
  * Wath ，用于实现 shutdown 等
@@ -18,9 +17,9 @@ public class NothingZookeeperWatch implements Watcher{
 	
 	@Override
 	public void process(WatchedEvent event) {
-		if(event.getState() == KeeperState.SyncConnected){
-			
-		}
+//		if(event.getState() == KeeperState.SyncConnected){
+//			
+//		}
 	}
 
 }
