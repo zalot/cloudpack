23fca912bb174ad4825b1e4bec2619a5d761e35d
zalot.zhaoh <zalot.zhaoh@766b8ed6-8c02-485e-ac38-e4d67e22fedb>
2012-03-13 10:29:36 +0000

    git-svn-id: http://svn.alibaba-inc.com/repos/ali_platform/havana/sandbox/trunk@154825 766b8ed6-8c02-485e-ac38-e4d67e22fedb

--- src/main/assembly/bin/mvninstall.sh
+++ src/main/assembly/bin/mvninstall.sh
@@ -1,1 +1,4 @@
-mvn install:install-file -Dfile=../lib/hbase-0.92.0-tests.jar -DgroupId=com.alibaba.external -DartifactId=apache.hadoop.hbase.test -Dversion=0.92.0 -Dpackaging=jar -DgeneratePom=true -DupdateReleaseInfo=true
+mvn install:install-file -Dfile=../lib/hbase-0.92.0-tests.jar -DgroupId=com.alibaba.external -DartifactId=apache.hadoop.hbase.test -Dversion=0.92.0 -Dpackaging=jar -DgeneratePom=true -DupdateReleaseInfo=true $1
+mvn install:install-file -Dfile=../lib/hbase-test-1.0.0.jar -DgroupId=com.alibaba.external -DartifactId=apache.hadoop.test -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true -DupdateReleaseInfo=true $1
+mvn install:install-file -Dfile=../lib/guava-r09.jar -DgroupId=com.alibaba.external -DartifactId=google.common -Dversion=0.9 -Dpackaging=jar -DgeneratePom=true -DupdateReleaseInfo=true $1
+mvn install:install-file -Dfile=../lib/high-scale-lib-1.1.1.jar -DgroupId=com.alibaba.external -DartifactId=github.highscalelib -Dversion=1.1.1 -Dpackaging=jar -DgeneratePom=true -DupdateReleaseInfo=true $1
