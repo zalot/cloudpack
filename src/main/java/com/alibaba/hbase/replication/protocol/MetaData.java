4bf40156f735e71df6df5f143c1cf76faf499445
zalot.zhaoh <zalot.zhaoh@766b8ed6-8c02-485e-ac38-e4d67e22fedb>
2012-03-16 03:27:42 +0000

    git-svn-id: http://svn.alibaba-inc.com/repos/ali_platform/havana/sandbox/trunk@156166 766b8ed6-8c02-485e-ac38-e4d67e22fedb

--- src/main/java/com/alibaba/hbase/replication/protocol/MetaData.java
+++ src/main/java/com/alibaba/hbase/replication/protocol/MetaData.java
@@ -1,8 +1,12 @@
 package com.alibaba.hbase.replication.protocol;
 
 public interface MetaData {
-	public Head getHead();
-	public Body getBody();
-	public byte[] getBodyData();
-	public void setBodyData(byte[] data);
+
+    public Head getHead();
+
+    public Body getBody();
+
+    public byte[] getBodyData();
+
+    public void setBodyData(byte[] data);
 }
