b00c5cd6b0d4ceaf34772b3e7d478b7e233f6112
zalot.zhaoh <zalot.zhaoh@766b8ed6-8c02-485e-ac38-e4d67e22fedb>
2012-03-22 07:11:46 +0000

    git-svn-id: http://svn.alibaba-inc.com/repos/ali_platform/havana/sandbox/trunk@158217 766b8ed6-8c02-485e-ac38-e4d67e22fedb

--- src/main/java/com/alibaba/hbase/replication/protocol/Body.java
+++ src/main/java/com/alibaba/hbase/replication/protocol/Body.java
@@ -2,6 +2,12 @@ package com.alibaba.hbase.replication.protocol;
 
 import org.apache.hadoop.hbase.KeyValue;
 
+/**
+ * 基础协议体 <BR>
+ * 
+ * 类Body.java的实现描述：TODO 类实现描述 
+ * @author zalot.zhaoh Mar 22, 2012 3:11:22 PM
+ */
 public abstract class Body {
 
     public abstract void setBodyData(byte[] data) throws Exception;
